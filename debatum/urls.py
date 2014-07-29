19b2d3f314bf4dd8ecc4a165fe5353069ea6cfac
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-07-29 16:36:04 -0700

    Adds correct urls

--- debatum/urls.py
+++ debatum/urls.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^polls/', include('polls.urls')),
-	url(r'^admin/', include(admin.site.urls))
+	# ex: /polls/
+	url(r'^polls/$', views.index, name='index'),
+	# ex: /polls/5/
+    url(r'^polls/(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	# ex: /polls/5/results/
+	url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+	# ex: /polls/5/vote/
+	url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
 )
--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('Hello! You are at the index')
+	return HttpResponse("Hello! You are at the index wats")
+
+def detail(request, poll_id):
+	return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("You're looking at the results of poll %s" % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("You're voting on poll %s" % poll_id)
